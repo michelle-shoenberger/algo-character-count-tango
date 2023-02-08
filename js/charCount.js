2172f905fd43c441a03cbadeaee24bbdf3e4c207
mickey <shoey25ms@gmail.com>
2023-02-07 19:11:16 -0500

    Update

--- js/charCount.js
+++ js/charCount.js
@@ -1,3 +1,29 @@
-exports.charCount = function() {
-
+exports.charCount = function(s) {
+  let count = {};
+  for (const char of s.toLowerCase()) {
+    if (char in count) {
+      count[char] += 1
+    } else if (char == " ") {
+      continue
+    } else {
+      count[char] = 1
+    }
+  }
+  console.log(count)
+  arr = Object.entries(count).sort(function(a,b) {
+    // Comparing two arrays : [char, count]
+    if (a[1] > b[1]) {
+      return -1;
+    } else if (a[1] == b[1]) {
+      if (a<b) {
+        return -1;
+      } else {
+        return 1;
+      }
+    } else {
+      return 1
+    }
+  })
+  console.log(arr)
+  return arr
 };
--- python/char_count.py
+++ python/char_count.py
@@ -1,2 +1,21 @@
-def char_count(str):
-  # Your code here
+def comparator(a, b):
+  if a[1]>b[1]:
+    return -1
+  elif a[1]==b[1]:
+    if a<b:
+      return 1
+    else:
+      return -1
+  else:
+    return 1
+
+def char_count(s):
+  count = {}
+  for char in s.lower():
+    if char.isspace():
+      continue
+    count[char] = count[char] + 1 if char in count else 1
+  print(count)
+  ans = sorted([[char, num] for (char, num) in count.items()], key=lambda x: (-x[1], x[0]))
+  print(ans)
+  return ans
